d9bcacecda754eb151a86b995355f91445a83cb1
java <134158550+LuaGunsX@users.noreply.github.com>
2026-04-02 17:27:59 -0400

    fixes by sloppey

--- revproxywrap.lua
+++ revproxywrap.lua
@@ -3,13 +3,19 @@ local Revproxy = {}
 function Revproxy:InsertRBXM(id, par)
    local asset = game:HttpGet("https://proxy.95.fyi/fetchasset.php?assetId="..id, true)
    writefile("RevProxy"..id..".rbxm", asset, true)
-   local content = getelyasset("RevProxy"..id..".rbxm").Parent = par
+   local content = getelyasset("RevProxy"..id..".rbxm")
+   for _,v in ipairs(game:GetObjects(content)) do
+     v.Parent = par
+   end
 end
 
 function Revproxy:InsertRBXMX(id, par)
    local asset = game:HttpGet("https://proxy.95.fyi/fetchasset.php?assetId="..id, true)
    writefile("RevProxy"..id..".rbxm", asset)
-   local content = getelyasset("RevProxy"..id..".rbxm").Parent = par
+   local content = getelyasset("RevProxy"..id..".rbxm")
+   for _,v in ipairs(game:GetObjects(content)) do
+     v.Parent = par
+   end
 end
 
 return Revproxy
